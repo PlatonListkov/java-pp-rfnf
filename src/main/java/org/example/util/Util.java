789a5c06919149a55c536f84827481356955eb14
Platon Listkov <platon.listkov@gmail.com>
2023-10-27 20:49:58 +0300

    Practical task 1.1.4 completed.

---
+++ src/main/java/org/example/util/Util.java
@@ -0,0 +1,22 @@
+package org.example.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String BD_URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String BD_USERNAME = "root";
+    private static final String BD_PASSWORD = "root";
+
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(BD_URL, BD_USERNAME, BD_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
