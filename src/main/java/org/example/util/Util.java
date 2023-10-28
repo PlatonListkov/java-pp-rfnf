c66699eabef80c4e57865c4294b76a39f8901a7f
Platon Listkov <platon.listkov@gmail.com>
2023-10-28 13:59:46 +0300

    Util подготовлен

--- src/main/java/org/example/util/Util.java
+++ src/main/java/org/example/util/Util.java
@@ -1,15 +1,24 @@
 package org.example.util;
 
+import org.example.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String BD_DRIVER = "com.mysql.jdbc.Driver";
     private static final String BD_URL = "jdbc:mysql://localhost:3306/mydbtest";
     private static final String BD_USERNAME = "root";
     private static final String BD_PASSWORD = "root";
-
+    private static final String BD_DIALECT = "org.hibernate.dialect.MySQL5Dialect";
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
         Connection connection = null;
@@ -19,5 +28,36 @@ public class Util {
             e.printStackTrace();
         }
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, BD_DRIVER);
+                settings.put(Environment.URL, BD_URL);
+                settings.put(Environment.USER, BD_USERNAME);
+                settings.put(Environment.PASS, BD_PASSWORD);
+                settings.put(Environment.DIALECT, BD_DIALECT);
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
