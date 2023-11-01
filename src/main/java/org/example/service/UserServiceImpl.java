0895ba100484d4a9e3dbe81b884188796cfa9ddd
Platon Listkov <platon.listkov@gmail.com>
2023-11-01 10:31:19 +0300

    Main обращается к UserService. UserService создает экземпляр UserDao и работает с ним.'

--- src/main/java/org/example/service/UserServiceImpl.java
+++ src/main/java/org/example/service/UserServiceImpl.java
@@ -1,38 +1,42 @@
 package org.example.service;
 
+import org.example.dao.UserDao;
+import org.example.dao.UserDaoJDBCImpl;
 import org.example.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
+    UserDao userDao = new UserDaoJDBCImpl();
+
     @Override
     public void createUsersTable() {
-
+        userDao.createUsersTable();
     }
 
     @Override
     public void dropUsersTable() {
-
+        userDao.dropUsersTable();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        userDao.saveUser(name, lastName, age);
     }
 
     @Override
     public void removeUserById(long id) {
-
+        userDao.removeUserById(id);
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        return userDao.getAllUsers();
     }
 
     @Override
     public void cleanUsersTable() {
-
+        userDao.cleanUsersTable();
     }
 }
